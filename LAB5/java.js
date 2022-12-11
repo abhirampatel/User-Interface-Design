eb88a134a955e9057e5ec5127386725b727a14be
abhirampatel <116938505+abhirampatel@users.noreply.github.com>
2022-12-11 15:00:56 +0530

    Add files via upload

---
+++ LAB5/java.js
@@ -0,0 +1,36 @@
+function clicked(){
+    if(img==1){
+        document.getElementById("img").style.display="inline"
+        return img=0
+    }else{
+        document.getElementById("img").style.display="none"
+        return img=1
+    }
+}
+function clicked1(){
+    if(img1==1){
+      document.getElementById("img1").style.display="inline"
+      return img1=0
+    }else{
+      document.getElementById("img1").style.display="none"
+      return img1=1
+    }
+  }
+  function clicked2(){
+    if(img2==1){
+      document.getElementById("img2").style.display="inline"
+      return img2=0
+    }else{
+      document.getElementById("img2").style.display="none"
+      return img2=1
+    }
+  }
+  function clicked3(){
+    if(img3==1){
+      document.getElementById("img3").style.display="inline"
+      return img3=0
+    }else{
+      document.getElementById("img3").style.display="none"
+      return img3=1
+    }
+  }
